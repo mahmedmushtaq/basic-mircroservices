d775daff2cb34a6673b106908e62766f3e1d2c7e
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-08-24 19:59:11 +0500

    Before docker implementation

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,19 +14,58 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending',
+    },
+  });
 
   res.status(201).send(comments);
 });
 
+app.post('/events', async (req, res) => {
+  console.log('event received', req.body.type);
+
+  console.log('req body is = ', req.body);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    console.log('status is = ', status);
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((cmnt) => cmnt.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
 app.listen(4001, () => {
-  console.log('Listening on 4001');
+  console.log('Comments is listening on 4001');
 });
---
+++ query/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+const handleEvents = async (type, data) => {
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, status, postId, content } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((cmnt) => cmnt.id === id);
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get('/posts', async (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvents(type, data);
+
+  res.send({});
+});
+
+app.listen(4002, async () => {
+  console.log('query is listening on the port 4002');
+  const res = await axios.get('http://localhost:4005/events');
+  console.log('res data is = ', res.data);
+  res.data.map((data) => {
+    handleEvents(data.type, data.data);
+  });
+});
